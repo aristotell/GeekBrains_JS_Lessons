311bb8c0957b765337c0e1f9a3fc5901a833b8e9
Ostap.K <ostapkulyk95@gmail.com>
2018-10-20 18:39:39 +0200

    Corrections

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
     // Homework Nº1
 
     var Tc = 44;
-    var Tf = ((9 / 5) * Tc + 32) // formula
+    var Tf = ((9 / 5) * Tc + 32); // formula
     console.log(Tf + ' °F');
-    alert(Tf + "ºF");
+    alert(Tf + 'ºF');
 
     // Homework Nº2
 
 
-    var name = "Василий";
+    var name = 'Василий';
     var admin = name;
     console.log(admin);
     alert(admin = name);
 
     // Homework Nº3
     
-    alert(1000 + "108");
-    console.log(1000 + "108");
+    alert(1000 + '108');
+    console.log(1000 + '108');
 
     //"<script defer>" откладывает выполнение скрипта до тех пор, пока вся страница не будет загружена полностью
     //"<script async>" Скачает вместе, выполнение в том порядке, в котором они будут загружены.
