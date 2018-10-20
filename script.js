f85ec4f668f57358f2ec43aa8cd39b1ed9431321
Ostap.K <ostapkulyk95@gmail.com>
2018-10-20 13:54:16 +0200

    Снял закоментирование с alert

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     var Tc = 44;
     var Tf = ((9 / 5) * Tc + 32) // formula
     console.log(Tf + ' °F');
-    // alert(Tf + "ºF");
+    alert(Tf + "ºF");
 
     // Homework Nº2
 
@@ -11,10 +11,11 @@
     var name = "Василий";
     var admin = name;
     console.log(admin);
-    //alert(admin = name);
+    alert(admin = name);
 
     // Homework Nº3
-
+    
+    alert(1000 + "108");
     console.log(1000 + "108");
 
     //"<script defer>" откладывает выполнение скрипта до тех пор, пока вся страница не будет загружена полностью
