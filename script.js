9ea5a714d952f73ee1f9304024a68d41a5179cb3
Ostap.K <ostapkulyk95@gmail.com>
2018-10-20 12:35:58 +0200

    Lesson Nº1 JavaScript

---
+++ script.js
@@ -0,0 +1,21 @@
+    // Homework Nº1
+
+    var Tc = 44;
+    var Tf = ((9 / 5) * Tc + 32) // formula
+    console.log(Tf + ' °F');
+    // alert(Tf + "ºF");
+
+    // Homework Nº2
+
+
+    var name = "Василий";
+    var admin = name;
+    console.log(admin);
+    //alert(admin = name);
+
+    // Homework Nº3
+
+    console.log(1000 + "108");
+
+    //"<script defer>" откладывает выполнение скрипта до тех пор, пока вся страница не будет загружена полностью
+    //"<script async>" Скачает вместе, выполнение в том порядке, в котором они будут загружены.
